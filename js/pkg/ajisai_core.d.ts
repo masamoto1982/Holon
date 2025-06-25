a1fa7606a49e2e401ec9c3156707d3541a421d2a
GitHub Action <action@github.com>
2025-06-25 14:40:48 +0000

    Add WASM build artifacts

---
+++ js/pkg/ajisai_core.d.ts
@@ -0,0 +1,54 @@
+/* tslint:disable */
+/* eslint-disable */
+export class AjisaiInterpreter {
+  free(): void;
+  constructor();
+  execute(code: string): string;
+  get_stack(): any;
+  get_register(): any;
+  get_custom_words(): string[];
+  reset(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_ajisaiinterpreter_free: (a: number, b: number) => void;
+  readonly ajisaiinterpreter_new: () => number;
+  readonly ajisaiinterpreter_execute: (a: number, b: number, c: number) => [number, number, number, number];
+  readonly ajisaiinterpreter_get_stack: (a: number) => any;
+  readonly ajisaiinterpreter_get_register: (a: number) => any;
+  readonly ajisaiinterpreter_get_custom_words: (a: number) => [number, number];
+  readonly ajisaiinterpreter_reset: (a: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __externref_table_dealloc: (a: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __externref_drop_slice: (a: number, b: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ js/pkg/ajisai_core_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_ajisaiinterpreter_free: (a: number, b: number) => void;
+export const ajisaiinterpreter_new: () => number;
+export const ajisaiinterpreter_execute: (a: number, b: number, c: number) => [number, number, number, number];
+export const ajisaiinterpreter_get_stack: (a: number) => any;
+export const ajisaiinterpreter_get_register: (a: number) => any;
+export const ajisaiinterpreter_get_custom_words: (a: number) => [number, number];
+export const ajisaiinterpreter_reset: (a: number) => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_start: () => void;
