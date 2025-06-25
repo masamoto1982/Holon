610dd6f963b97e64b45434f87756c012044df196
masamoto yamashiro <masamoto@protonmail.com>
2025-06-25 10:21:27 +0900

    Update lib.rs

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,41 +1,103 @@
 use wasm_bindgen::prelude::*;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+mod types;
+mod tokenizer;
+mod interpreter;
+mod builtins;
+
+use types::*;
+use tokenizer::*;
+use interpreter::*;
 
 #[wasm_bindgen]
-pub struct Fraction {
-    numerator: i64,
-    denominator: i64,
+pub struct AjisaiInterpreter {
+    interpreter: Interpreter,
 }
 
 #[wasm_bindgen]
-impl Fraction {
+impl AjisaiInterpreter {
     #[wasm_bindgen(constructor)]
-    pub fn new(numerator: i64, denominator: i64) -> Fraction {
-        let gcd = Self::gcd(numerator.abs(), denominator.abs());
-        let mut num = numerator / gcd;
-        let mut den = denominator / gcd;
-        
-        if den < 0 {
-            num = -num;
-            den = -den;
-        }
-        
-        Fraction {
-            numerator: num,
-            denominator: den,
+    pub fn new() -> Self {
+        AjisaiInterpreter {
+            interpreter: Interpreter::new(),
         }
     }
-    
-    fn gcd(a: i64, b: i64) -> i64 {
-        if b == 0 { a } else { Self::gcd(b, a % b) }
+
+    #[wasm_bindgen]
+    pub fn execute(&mut self, code: &str) -> Result<String, String> {
+        match self.interpreter.execute(code) {
+            Ok(()) => Ok("OK".to_string()),
+            Err(e) => Err(e.to_string()),
+        }
     }
-    
-    #[wasm_bindgen(getter)]
-    pub fn numerator(&self) -> i64 {
-        self.numerator
+
+    #[wasm_bindgen]
+    pub fn get_stack(&self) -> Result<JsValue, JsValue> {
+        let stack_values: Vec<JsValue> = self.interpreter
+            .get_stack()
+            .iter()
+            .map(|v| value_to_js(v))
+            .collect();
+        Ok(JsValue::from_serde(&stack_values).unwrap())
     }
-    
-    #[wasm_bindgen(getter)]
-    pub fn denominator(&self) -> i64 {
-        self.denominator
+
+    #[wasm_bindgen]
+    pub fn get_register(&self) -> JsValue {
+        match self.interpreter.get_register() {
+            Some(v) => value_to_js(v),
+            None => JsValue::NULL,
+        }
     }
+
+    #[wasm_bindgen]
+    pub fn get_custom_words(&self) -> Vec<String> {
+        self.interpreter.get_custom_words()
+    }
+
+    #[wasm_bindgen]
+    pub fn reset(&mut self) {
+        self.interpreter = Interpreter::new();
+    }
+}
+
+fn value_to_js(value: &Value) -> JsValue {
+    let obj = js_sys::Object::new();
+    
+    let type_str = match &value.val_type {
+        ValueType::Number(_) => "number",
+        ValueType::String(_) => "string",
+        ValueType::Boolean(_) => "boolean",
+        ValueType::Symbol(_) => "symbol",
+        ValueType::Vector(_) => "vector",
+        ValueType::Nil => "nil",
+    };
+    
+    js_sys::Reflect::set(&obj, &"type".into(), &type_str.into()).unwrap();
+    
+    let val = match &value.val_type {
+        ValueType::Number(n) => {
+            if n.denominator == 1 {
+                n.numerator.into()
+            } else {
+                format!("{}/{}", n.numerator, n.denominator).into()
+            }
+        },
+        ValueType::String(s) => s.clone().into(),
+        ValueType::Boolean(b) => (*b).into(),
+        ValueType::Symbol(s) => s.clone().into(),
+        ValueType::Vector(v) => {
+            let arr = js_sys::Array::new();
+            for item in v.iter() {
+                arr.push(&value_to_js(item));
+            }
+            arr.into()
+        },
+        ValueType::Nil => JsValue::NULL,
+    };
+    
+    js_sys::Reflect::set(&obj, &"value".into(), &val).unwrap();
+    
+    obj.into()
 }
